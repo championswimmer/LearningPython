04259cbad3b8fca6c091c0dd126e359ceced4619
Arnav Gupta <arnav.gupta@cube26.com>
2014-07-27 20:23:50 +0530

    parsing json data and going through the loop

--- LearningPython/http_get_json/__init__.py
+++ LearningPython/http_get_json/__init__.py
@@ -1,8 +1,14 @@
 import requests;
+import json;
+
 from http_get_json.constants import  *
 
 print(str(url));
 
-r = requests.get(url);
-data = r.json()
-print (data)
+response = requests.get(url);
+
+jsonData = json.loads(response.text)
+
+for item in jsonData:
+    print("EMAIL : " + item.get('email') + "\n" )
+    
