530edd6014885788d9af4154c389579384aa7235
Arnav Gupta <arnav.gupta@cube26.com>
2014-07-27 20:41:35 +0530

    learing all the package and module bullshit

---
+++ LearningPython/http_get_json/carpool_emails.py
@@ -0,0 +1,19 @@
+'''
+Created on 27-Jul-2014
+
+@author: arnav
+'''
+import requests;
+import json;
+
+from http_get_json.constants import  *
+
+print(str(url));
+
+response = requests.get(url);
+
+jsonData = json.loads(response.text)
+
+for item in jsonData:
+    print("EMAIL : " + item.get('email') + "\n" )
+    
